7ff4ade05f67559068030a646d52a7b100ff6573
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-17 19:54:54 +0530

    navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Logo from '../assets/pizzaLogo.png'
+import {Link} from "react-router-dom";
+import ReorderIcon from '@material-ui/icons/Reorder';
+import "../styles/Navbar.css";
 
 function Navbar() {
     return (
@@ -7,7 +10,15 @@ function Navbar() {
             <div className='leftSide'>
                 <img src={Logo} />
             </div>
-            <div className='rightSide'></div>
+            <div className='rightSide'>
+                <Link to="/">Home</Link>
+                <Link to="/menu">Menu</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+                <button>
+                    <ReorderIcon/>
+                </button>
+            </div>
         </div>
     );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            hei
+        </div>
+    )
+}
+
+export default Home

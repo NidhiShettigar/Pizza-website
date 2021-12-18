0ff32c60d6dd9a0bcc296abcb1c79b773d55096a
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-18 22:18:35 +0530

    home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerImage from "../assets/pizza.jpeg";
+import "../styles/Home.css";
 
 function Home() {
     return (
-        <div>
-            hei
+        <div className='home'>
+            <div className='headerContainer' 
+                 style={{backgroundImage: `url(${BannerImage})`}}>
+                <h1>Pedro's Pizza</h1>
+                <p>PIZZA TO FIT ANY TASSTE</p>
+                <Link to='/menu'>
+                    <button>ORDER NOW</button>
+                </Link>
+            </div>
         </div>
     )
 }

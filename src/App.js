e128f3c0364f6663dceeb818036da0100b45ab5b
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2021-12-20 22:08:13 +0530

    home page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Home from "./pages/Home"
+import Footer from './components/Footer';
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
@@ -10,7 +12,9 @@ function App() {
         <Navbar/>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
         </Switch>
+        <Footer />
       </Router>
       
     </div>
